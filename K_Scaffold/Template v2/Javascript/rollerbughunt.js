ce83d8877aa383ad0f10d2c29988c5e2e5729b64
Kurohyou <scaseydv@gmail.com>
2022-08-07 09:16:05 -0600

    Tests for bug #26

---
+++ K_Scaffold/Template v2/Javascript/rollerbughunt.js
@@ -0,0 +1,11 @@
+/**
+ * Tests roll buttons
+ * @param {object} trigger - The trigger that caused the function to be called
+ * @param {object} attributes - The attribute values of the character
+ * @param {object[]} sections - All the repeating section IDs
+ * @param {object} casc - Expanded cascade object
+ */
+const testButtons = function({trigger,attributes,sections,casc}){
+  console.log({buttontrigger:trigger});
+};
+k.registerFuncs({testButtons});
