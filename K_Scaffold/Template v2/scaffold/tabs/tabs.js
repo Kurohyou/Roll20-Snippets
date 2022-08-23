22201ede250cfc3773e0673f7682665f3bc0b2b9
Riernar <Riernar@users.noreply.github.com>
2022-08-23 20:33:25 +0200

    integrated feedback

--- K_Scaffold/Template v2/scaffold/tabs/tabs.js
+++ K_Scaffold/Template v2/scaffold/tabs/tabs.js
@@ -1,17 +1,12 @@
 
-(
-    function registerTabHandlers(storage) {
-        storage.forEach(
-            ({ name, tabs }) => {
-                kFuncs.log(`Registering handler for tab "${name}"`);
-                $20(`.tab.tab-${name} .tab-header.tab-header-${name} .tab-button.tab-button-${name}`).on("click", (event) => {
-                    const container_tab = event.htmlAttributes["data-container-tab"];
-                    const tab = event.htmlAttributes["data-tab"];
-                    kFuncs.log(`Clicked tab ${tab} in ${container_tab}`);
-                    $20(`.tab.tab-${name} div[data-tab]`).removeClass("active");
-                    $20(`.tab.tab-${name} div[data-tab=${tab}]`).addClass("active");
-                });
-            }
-        );
-    }
-)(tabDetails);
+const _kSwitchTab = function ({ trigger }) {
+    const [container, tab] = (
+        trigger.name.match(/nav-tabs-(.+)--(.+)/) ||
+        []
+    ).slice(1);
+    console.log([container, tab]);
+    $20(`[data-container-tab="${container}"]`).removeClass('k-active-tab');
+    $20(`[data-container-tab="${container}"][data-tab="${tab}"]`).addClass('k-active-tab');
+}
+
+registerFuncs({ _kSwitchTab });
