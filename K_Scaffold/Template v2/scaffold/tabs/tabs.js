43fa17a4e23c08801f9a6be3238738883743a308
Kurohyou <scaseydv@gmail.com>
2022-08-23 15:44:42 -0600

    Modified tabs; updated documentation data

--- K_Scaffold/Template v2/scaffold/tabs/tabs.js
+++ K_Scaffold/Template v2/scaffold/tabs/tabs.js
@@ -1,12 +1,17 @@
-
+/**
+ * The default tab navigation function of the K-scaffold. Courtesy of Riernar. It will add `k-active-tab` to the active tab-container and `k-active-button` to the active button. You can either write your own CSS to control display of these, or use the default CSS included in `scaffold/_k.scss`. Note that `k-active-button` has no default CSS as it is assumed that you will want to style the active button to match your system.
+ * @param {Object} trigger - The trigger object
+ */
 const _kSwitchTab = function ({ trigger }) {
-    const [container, tab] = (
-        trigger.name.match(/nav-tabs-(.+)--(.+)/) ||
-        []
-    ).slice(1);
-    console.log([container, tab]);
-    $20(`[data-container-tab="${container}"]`).removeClass('k-active-tab');
-    $20(`[data-container-tab="${container}"][data-tab="${tab}"]`).addClass('k-active-tab');
+  const [container, tab] = (
+    trigger.name.match(/nav-tabs-(.+)--(.+)/) ||
+    []
+  ).slice(1);
+  console.log([container, tab]);
+  $20(`[data-container-tab="${container}"]`).removeClass('k-active-tab');
+  $20(`[data-container-tab="${container}"][data-tab="${tab}"]`).addClass('k-active-tab');
+  $20(`[data-container-button="${container}"]`).removeClass('k-active-button');
+  $20(`[data-container-button="${container}"][data-button="${tab}"]`).addClass('k-active-button');
 }
 
 registerFuncs({ _kSwitchTab });
