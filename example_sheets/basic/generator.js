a885bb3abefe1e95deae4f7f65dbce36a40eda34
Kurohyou <scaseydv@gmail.com>
2022-09-15 16:19:15 -0600

    Fleshing out example sheet documentation

--- example_sheets/basic/generator.js
+++ example_sheets/basic/generator.js
@@ -1,12 +1,5 @@
 const k = require('k-scaffold');
 
 (async ()=>{
-  const html = await k.pug({
-    source:'./basic.pug',
-    destination:'./roll20code/gen.html'
-  });
-  const css = await k.scss({
-    source:'./basic.scss',
-    destination: './roll20code/gen.css'
-  });
+  k.all({destination:'./roll20code/'});
 })();
