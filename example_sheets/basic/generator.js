2220464654bc0ed0cb8ceb1461b91d227a336359
Kurohyou <scaseydv@gmail.com>
2022-09-15 16:22:44 -0600

    Simplify generator.js for basic example

--- example_sheets/basic/generator.js
+++ example_sheets/basic/generator.js
@@ -1,5 +1,4 @@
-const k = require('k-scaffold');
+const k = require('k-scaffold');//Require the K-scaffold that we installed via NPM
 
-(async ()=>{
-  k.all({destination:'./roll20code/'});
-})();
+k.all({destination:'./roll20code/'});// Invoke the all method of the K-scaffold npm package to process all pug and scss files that are in the same directory as this file.
+// We've set our output directory as roll20code, located directly in this same directory.
